d2b5c0d63c11df154b6d54658876d70ce17cd031
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-09-12 17:56:11 -0300

    Classe responsavel pelo Threads , aonde faz todo o tratamento de dados de cada cliente, permitindo multi conexoes

--- src/main/java/pr/software/chat/ClienteHandler.java
+++ src/main/java/pr/software/chat/ClienteHandler.java
@@ -1,11 +1,13 @@
 package pr.software.chat;
-import pr.software.chat.Servidor;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * @author Paulo Ricardo Soares da Trindade
+ */
 public class ClienteHandler implements Runnable {
 
     private Socket socket;
